5e0243e25406eb40adb8968a3a24aa35ca9ff2cb
cdnphantom <cduynguyen95@gmail.com>
2019-11-24 22:11:59 -0500

    no routing build

---
+++ precache-manifest.af119184a172764c7a59be6fa3cc031d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e933dd5cc38d016cb76e07aed4be2b30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "162b52d19e741597a14a",
+    "url": "/static/css/main.4f71893c.chunk.css"
+  },
+  {
+    "revision": "74b277ea7dd93a848e09",
+    "url": "/static/js/2.9eead7e2.chunk.js"
+  },
+  {
+    "revision": "162b52d19e741597a14a",
+    "url": "/static/js/main.9d418fa3.chunk.js"
+  },
+  {
+    "revision": "93b29c31ca4299d643ba",
+    "url": "/static/js/runtime-main.4314199c.js"
+  }
+]);
