aadb94e33ada6ea84615b4068ebc367d99e4c0bc
cdnphantom <cduynguyen95@gmail.com>
2019-12-16 19:57:45 -0500

    Revert "zz"
    This reverts commit 5f3250c972930317c258857f3d95330dcca82818.

--- cuong-portfolio/src/components/App.js
+++ cuong-portfolio/src/components/App.js
@@ -3,7 +3,6 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Projects from "./Projects";
 import Profiles from "./Profiles";
-import {Switch, Route, Link, BrowserRouter} from "react-router-dom";
 
 const App = () => {
 
