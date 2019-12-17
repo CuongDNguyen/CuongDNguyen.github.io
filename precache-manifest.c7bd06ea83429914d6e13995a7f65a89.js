a52e9474e64aff90c0cf8159ed616bd49223302d
Cuong Nguyen <cduynguyen95@gmail.com>
2019-12-16 19:41:07 -0500

    ...

---
+++ precache-manifest.c7bd06ea83429914d6e13995a7f65a89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4bbe6887b81358eba8d686fff21aae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34ee8a214f47937d8408",
+    "url": "/static/css/main.4614d5a4.chunk.css"
+  },
+  {
+    "revision": "cd2eba8da35f28cdad99",
+    "url": "/static/js/2.fceab9ed.chunk.js"
+  },
+  {
+    "revision": "34ee8a214f47937d8408",
+    "url": "/static/js/main.5ea0c4a5.chunk.js"
+  },
+  {
+    "revision": "f295ce7e43b1bf3eef9e",
+    "url": "/static/js/runtime-main.cfb0a9d1.js"
+  }
+]);
