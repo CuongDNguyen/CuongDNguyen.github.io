b6304e283b5ff63ca3074add3f45be933df97aef
Cuong Nguyen <cduynguyen95@gmail.com>
2022-10-29 23:39:31 -0400

    bliss nails

--- cuong-portfolio/src/components/Projects.js
+++ cuong-portfolio/src/components/Projects.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import "../styles/Profiles.css";
+import ProfileContent from "./ProfileContent";
 
 const Projects = () => {
+
+    function ListItemLink(props) {
+        return <ListItem button component="a" {...props} />;
+    }
+
     return (
-        <div>
-            <href src="https://bliss-nails.vercel.app/">Bliss Nails</href>
-        </div>
+        <React.Fragment>
+            <List>
+                <ListItem>
+                    <ListItemLink href="https://bliss-nails.vercel.app/">
+                        <ListItemText primary="Bliss Nails"/>
+                    </ListItemLink>
+                </ListItem>
+            </List>
+            <ProfileContent/>
+        </React.Fragment>
     );
 };
 
