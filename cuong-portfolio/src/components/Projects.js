4748763ddad60697b99c24f34fc04480b63f81f0
Cuong Nguyen <cduynguyen95@gmail.com>
2022-10-29 23:36:36 -0400

    new project

--- cuong-portfolio/src/components/Projects.js
+++ cuong-portfolio/src/components/Projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Projects = () => {
     return (
         <div>
-            
+            <href src="https://bliss-nails.vercel.app/">Bliss Nails</href>
         </div>
     );
 };
